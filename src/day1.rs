f3d9a1454279e1c321da534d6e6bd24afe54295b
Barrett Flowers <barrettjflowers@gmail.com>
2024-12-12 18:21:45 -0500

    Day 1 readability

--- src/day1.rs
+++ src/day1.rs
@@ -1,23 +1,25 @@
 pub fn main() {
-    let i = std::fs::read_to_string("day1-input.txt").unwrap();
-    let mut l = Vec::new();
-    let mut r = Vec::new();
+    let input = std::fs::read_to_string("day1-input.txt").unwrap();
+    let mut lcolumn = Vec::new();
+    let mut rcolumn = Vec::new();
 
-    for line in i.lines() {
+    for line in input.lines() {
         let numbers: Vec<u32> = line
             .split_whitespace()
             .map(|s| s.parse().unwrap())
             .collect();
-        l.push(numbers[0]);
-        r.push(numbers[1]);
+        lcolumn.push(numbers[0]);
+        rcolumn.push(numbers[1]);
     }
-    l.sort();
-    r.sort();
 
-    let sum: u32 = l
+    lcolumn.sort();
+    rcolumn.sort();
+
+    let sum: u32 = lcolumn
         .iter()
-        .zip(r.iter())
+        .zip(rcolumn.iter())
         .map(|(left, right)| right.abs_diff(*left))
         .sum();
-    println!("Day 1 part 1: {}", sum);
+
+    println!("Day 1: {}", sum);
 }
